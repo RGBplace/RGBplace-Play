f0f8a4db2844898beb4a184f89421bdc5a72c5b3
Daehyun Im <scallop99@godo.co.kr>
2019-10-01 16:43:46 +0900

    Initial dist subtree commit

---
+++ dist/precache-manifest.b839f8d7b7f6713cdf10543d75a35a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c8bd82de5196a82c627d",
+    "url": "/js/chunk-vendors.329b550b.js"
+  },
+  {
+    "revision": "e2fcc77ed29ce9771c59",
+    "url": "/js/app.26d017a8.js"
+  },
+  {
+    "revision": "8030d87d38c52435fa9ff9ddee52acbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ac6801780f55c1a0a86f49f86a1c22",
+    "url": "/img/RGB_place.png"
+  },
+  {
+    "revision": "c8bd82de5196a82c627d",
+    "url": "/css/chunk-vendors.172097aa.css"
+  },
+  {
+    "revision": "e2fcc77ed29ce9771c59",
+    "url": "/css/app.92eda7fe.css"
+  }
+];
