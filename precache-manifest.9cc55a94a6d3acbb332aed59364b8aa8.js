41da1705262016af61d142a35da21de5afc8e31e
Daehyun Lim <6lueparr0t@gmail.com>
2020-02-13 13:49:34 +0900

    auto commit 2020-02-13 13:44:21

---
+++ precache-manifest.9cc55a94a6d3acbb332aed59364b8aa8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77eb00970ee563d3c1f2",
+    "url": "/css/Home.207437e9.css"
+  },
+  {
+    "revision": "22ef35bf6619fb5bbbe9",
+    "url": "/css/app.15e34309.css"
+  },
+  {
+    "revision": "f9dab8f1001a9a2d4541",
+    "url": "/css/chunk-vendors.eac1fad9.css"
+  },
+  {
+    "revision": "1dff2d85e66398ee2904147ddafc747e",
+    "url": "/img/000. Rolling Stone Magazine's 500 Greatest Songs Of All Time1.jpg"
+  },
+  {
+    "revision": "23ac6801780f55c1a0a86f49f86a1c22",
+    "url": "/img/RGB_place.png"
+  },
+  {
+    "revision": "47b731f5a8f9d6eda243fdcaefd039af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77eb00970ee563d3c1f2",
+    "url": "/js/Home.2021241a.js"
+  },
+  {
+    "revision": "22ef35bf6619fb5bbbe9",
+    "url": "/js/app.f9898caf.js"
+  },
+  {
+    "revision": "f9dab8f1001a9a2d4541",
+    "url": "/js/chunk-vendors.a27a197d.js"
+  },
+  {
+    "revision": "620ccc4799f4046217f9f0af123bdebb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
